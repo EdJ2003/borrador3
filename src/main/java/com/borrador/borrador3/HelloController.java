c918e49061a6f4bebe5fe52fa63f625e08f7cce0
Edj2003 <eduenflma@unisabana.edu.co>
2022-10-19 12:51:09 -0500

    Nuevos cambios

---
+++ src/main/java/com/borrador/borrador3/HelloController.java
@@ -0,0 +1,14 @@
+package com.borrador.borrador3;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
